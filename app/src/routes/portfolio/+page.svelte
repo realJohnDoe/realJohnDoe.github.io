3968872e7a65473acb34092873008ec7a326de77
Johannes Döllinger <johannes.doellinger@gmail.com>
2024-08-18 14:00:55 +0200

    fix site

--- app/src/routes/portfolio/+page.svelte
+++ app/src/routes/portfolio/+page.svelte
@@ -2,6 +2,8 @@
 	import MainLayout from '$lib/MainLayout.svelte';
 	import kenpoImg from '$lib/images/kenpo-notes.png';
 	import feelbackImg from '$lib/images/feelback-summary.png';
+	import gitHubIcon from '$lib/images/github.svg';
+	import gitLabIcon from '$lib/images/gitlab.svg';
 </script>
 
 <svelte:head>
@@ -13,17 +15,6 @@
 	<h2 class="text-2xl pb-3">Portfolio</h2>
 
 	<div class="grid sm:grid-cols-2 gap-2">
-		<a href="https://realjohndoe.github.io/kenpo-quartz/" target="_blank">
-			<div class="card shadow-md">
-				<figure>
-					<img src={kenpoImg} alt="kenpo" loading="lazy" />
-				</figure>
-				<div class="card-body">
-					<h3 class="text-lg font-semibold">Kenpo Notes</h3>
-					<p class="project-category">Structuring my Dojo's Kenpo Content</p>
-				</div>
-			</div>
-		</a>
 		<a
 			href="https://play.google.com/store/apps/details?id=com.johndoe_software.feelback"
 			target="_blank"
@@ -34,8 +25,37 @@
 				</figure>
 
 				<div class="card-body">
-					<h3 class="text-lg font-semibold">Feelback</h3>
-					<p class="project-category">Privacy-friendly Emotion Tracker for Android</p>
+					<div class="flex">
+						<div class="grow pe-5">
+							<h3 class="text-lg font-semibold">Feelback</h3>
+							<p class="project-category">Privacy-friendly Emotion Tracker for Android</p>
+						</div>
+						<a href="https://gitlab.com/jdcoding/feelback" target="_blank">
+							<button class="btn btn-square btn-primary">
+								<img src={gitLabIcon} alt="Location" />
+							</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</a>
+		<a href="https://realjohndoe.github.io/kenpo-quartz/" target="_blank">
+			<div class="card shadow-md">
+				<figure>
+					<img src={kenpoImg} alt="kenpo" loading="lazy" />
+				</figure>
+				<div class="card-body">
+					<div class="flex">
+						<div class="grow pe-5">
+							<h3 class="text-lg font-semibold">Kenpo Notes</h3>
+							<p class="project-category">Structuring my Dojo's Kenpo Content</p>
+						</div>
+						<a href="https://github.com/realJohnDoe/kenpo-quartz" target="_blank">
+							<button class="btn btn-square btn-primary">
+								<img class="w-6" src={gitHubIcon} alt="Location" />
+							</button>
+						</a>
+					</div>
 				</div>
 			</div>
 		</a>
