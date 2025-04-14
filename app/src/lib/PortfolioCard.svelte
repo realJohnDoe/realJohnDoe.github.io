d23f23f514c4846103b94e571741c9469023bc5b
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-04-14 22:37:08 +0200

    Improve Side Project card layout

--- app/src/lib/PortfolioCard.svelte
+++ app/src/lib/PortfolioCard.svelte
@@ -11,27 +11,26 @@
 <div class="card bg-base-300 shadow-xl">
 	<figure class="relative">
 		<img src={image} alt={title} loading="lazy" />
-		<div class="absolute w-full bottom-0 right-0 p-4 bg-gradient-to-t from-base-300 to-transparent">
-			<!-- Overlay for links -->
-			<div class="flex gap-2 justify-end">
+	</figure>
+	<div class="card-body flex flex-col">
+		<div class="flex justify-between items-end">
+			<!-- Row for title and links -->
+			<h3 class="text-lg font-semibold">{title}</h3>
+			<div class="flex gap-2">
 				<!-- Section for links -->
 				{#each links as link}
 					<a href={link.href} target="_blank">
-						<button class="btn btn-square btn-neutral shadow-2xl">
+						<button
+							class="btn btn-square btn-neutral shadow-2xl hover:shadow-xl transition-shadow duration-200"
+						>
+							<!-- Updated shadow classes -->
 							<img class="w-6" src={link.icon} alt={link.text} />
 						</button>
 					</a>
 				{/each}
 			</div>
 		</div>
-	</figure>
-	<div class="card-body flex flex-col">
-		<div class="flex h-full">
-			<div class="grow pe-5">
-				<h3 class="text-lg font-semibold">{title}</h3>
-				<p class="project-category">{description}</p>
-			</div>
-		</div>
+		<p class="project-category pt-2">{description}</p>
 		<h2 class="text-md font-semibold">Technologies:</h2>
 		<div class="flex gap-2">
 			{#each technologies as tech}
