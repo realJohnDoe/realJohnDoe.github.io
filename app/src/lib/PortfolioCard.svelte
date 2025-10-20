979e06bf3c3b08f190f773f03a774b0988457e6e
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-10-20 16:11:41 +0200

    Align Portfolio Cards nicer

--- app/src/lib/PortfolioCard.svelte
+++ app/src/lib/PortfolioCard.svelte
@@ -8,34 +8,38 @@
 	export let technologies;
 </script>
 
-<div class="border border-primary rounded-xl">
-	<figure class="relative">
+<div class="border border-primary rounded-xl flex flex-col">
+	<figure class="relative flex-shrink-0">
 		<img src={image} alt={title} loading="lazy" />
 	</figure>
-	<div class="p-4 md:p-6 flex flex-col">
-		<div class="flex flex-col sm:flex-row sm:justify-between sm:items-end gap-2">
-			<!-- Row for title and links -->
-			<h3 class="text-lg font-semibold">{title}</h3>
-			<div class="flex gap-2">
-				<!-- Section for links -->
-				{#each links as link}
-					<a href={link.href} target="_blank">
-						<button
-							class="btn btn-square btn-neutral shadow-2xl hover:shadow-xl transition-shadow duration-200"
-						>
-							<!-- Updated shadow classes -->
-							<img class="w-6" src={link.icon} alt={link.text} />
-						</button>
-					</a>
+	<div class="p-4 md:p-6 flex flex-col h-full justify-between">
+		<div>
+			<div class="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
+				<!-- Row for title and links -->
+				<h3 class="text-lg font-semibold">{title}</h3>
+				<div class="flex gap-2">
+					<!-- Section for links -->
+					{#each links as link}
+						<a href={link.href} target="_blank">
+							<button
+								class="btn btn-square btn-neutral shadow-2xl hover:shadow-xl transition-shadow duration-200"
+							>
+								<!-- Updated shadow classes -->
+								<img class="w-6" src={link.icon} alt={link.text} />
+							</button>
+						</a>
+					{/each}
+				</div>
+			</div>
+			<p class="project-category pb-2 pt-4">{description}</p>
+		</div>
+		<div>
+			<h2 class="text-md font-semibold pb-2">Technologies:</h2>
+			<div class="flex gap-2 flex-wrap">
+				{#each technologies as tech}
+					<TechButton techButtonProps={tech} />
 				{/each}
 			</div>
-		</div>
-		<p class="project-category py-2">{description}</p>
-		<h2 class="text-md font-semibold pb-2">Technologies:</h2>
-		<div class="flex gap-2 flex-wrap">
-			{#each technologies as tech}
-				<TechButton techButtonProps={tech} />
-			{/each}
 		</div>
 	</div>
 </div>
